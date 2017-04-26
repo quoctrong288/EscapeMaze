70c4dd3feb68f956f3e87e6923c98451a20ea046
Quoc Trong Pham <quoctrong.qb@gmail.com>
2017-04-26 09:58:19 +0700

    first commit

---
+++ Assets/Scripts/LoadMap.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMap : MonoBehaviour
+{
+
+    public GameObject wall;
+
+    const int ROWS = 16;
+    const int COLUMNS = 11;
+
+    int[,] map = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1},
+    {1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1},
+    {1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1},
+    {1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1},
+    {1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1},
+    {1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1},
+    {1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} };
+
+    Vector3 StartPos = new Vector3(-2.5565f, 4.744f, 0);
+
+    void Start()
+    {
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                if (map[i, j] == 1)
+                {
+                    GameObject a = Instantiate(wall);
+                    a.transform.position = StartPos;
+                    StartPos += new Vector3(0.512f, 0, 0);
+                    a.transform.parent = this.transform;
+                }
+                else
+                {
+                    StartPos += new Vector3(0.512f, 0, 0);
+                }
+            }
+            StartPos -= new Vector3(0, 0.512f, 0);
+            StartPos.x = -2.5565f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
